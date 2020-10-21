b198cf87e17ec4839aa749244d39d55f1d185062
acse-jaq15 <72440238+acse-jaq15@users.noreply.github.com>
2020-10-21 18:57:15 +0100

    general updates

--- simple_functions/functions1.py
+++ simple_functions/functions1.py
@@ -1,6 +1,6 @@
 from functools import lru_cache
 
-__all__ = ['my_sum', 'factorial']
+__all__ = ['my_sum', 'factorial', 'sin_approx']
 
 
 def my_sum(iterable):
@@ -14,9 +14,8 @@ def my_sum(iterable):
 def factorial(n):
     return n * factorial(n-1) if n else 1
 
-def sin_approx(x,k=20):
+def sin_approx(x,k=10):
     sin_approx = 0
     for i in range(k):
-        sign = (-1)**i
         sin_approx += (((-1)**i)*x**(1+2*i))/factorial(1+2*i)
     return sin_approx
